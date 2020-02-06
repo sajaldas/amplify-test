2fb7515089c17a026d6484c37d873729b37976d5
sajal <sajal@vrtechnologies.info>
2020-02-06 12:04:54 +0530

    Initial commit with autogenerated amplify forms.

---
+++ src/SignUp.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            phone: ''
+        }
+    }
+
+    render() {
+        return {
+            
+        }
+    }
+
+}
+
+
+export default SignUp
