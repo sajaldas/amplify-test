3080bd06d284a2dd4c70ae7cba307a23dea8791c
sajal <sajal@vrtechnologies.info>
2020-02-07 16:37:36 +0530

    Updates

---
+++ precache-manifest.2fcf6728c0a02e87c451931462203774.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7215a0f671e27857242fbb73ce85b3",
+    "url": "/amplify-test/index.html"
+  },
+  {
+    "revision": "1aa8a875bc86ede168b0",
+    "url": "/amplify-test/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "7d19056467506d5efe25",
+    "url": "/amplify-test/static/css/main.e870755e.chunk.css"
+  },
+  {
+    "revision": "1aa8a875bc86ede168b0",
+    "url": "/amplify-test/static/js/2.42ad139a.chunk.js"
+  },
+  {
+    "revision": "6f38f7006eed5c015371dce65947d0c0",
+    "url": "/amplify-test/static/js/2.42ad139a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d19056467506d5efe25",
+    "url": "/amplify-test/static/js/main.73b5e59b.chunk.js"
+  },
+  {
+    "revision": "0b55aced89abe3457ae1",
+    "url": "/amplify-test/static/js/runtime-main.a446578c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amplify-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/amplify-test/precache-manifest.2fcf6728c0a02e87c451931462203774.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amplify-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
